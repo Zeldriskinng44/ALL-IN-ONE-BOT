d344787e31947d3c619da1b2ed10e97afc28f5d8
Zeldriskinng44 <144282751+Zeldriskinng44@users.noreply.github.com>
2025-02-14 17:46:22 +0100

    Update help.js

--- commands/basic/help.js
+++ commands/basic/help.js
@@ -34,7 +34,7 @@ module.exports = {
     async execute(interaction) {
         if (interaction.isCommand && interaction.isCommand()) {
         
-            const BOT_ICON = "https://cdn.discordapp.com/emojis/1334648756649590805.png";
+            const BOT_ICON = "https://i.pinimg.com/236x/43/4e/56/434e564fa2791d0104f915feebef2bdb.jpg";
             const EMBED_COLOR = "#3498db";
             const COMMANDS_DIR = path.join(__dirname, '../../commands');
             const EXCESS_COMMANDS_DIR = path.join(__dirname, '../../excesscommands');
